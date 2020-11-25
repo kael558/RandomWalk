09ace3675c74c6aa2b4693132188b48c8a6a00b5
Rahel Gunaratne <rahel.gunaratne@gmail.com>
2020-11-24 19:09:20 -0500

    logic working for both plots

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from random import uniform
-from math import pi, cos, sin
+from math import pi, cos, sin, sqrt
 
-figure = plt.figure()
+# Circle Parameters
+radius = 1
+point = (5, 5)
 
-
-
-
+# Simulation Parameters
 num_particles = 100
 num_steps = 100
+interval = 1000
+
+# Plot Setup
+figure = plt.figure()
 
 p1 = figure.add_subplot(211)
 xdata1, ydata1 = [0] * num_particles, [0] * num_particles
 ln1, = plt.plot([], [], 'bo')
 
 p2 = figure.add_subplot(212)
-xdata2, ydata2 = np.linspace(0, num_steps, num_steps+1), []
+xdata2, ydata2 = [], []
 ln2, = p2.plot([], [], 'go')
 
 
@@ -38,33 +42,37 @@ def init_p2():
 
 
 def update_p1(frame):
-    for particle in range(num_particles):
+    for p in range(num_particles):
         phi = uniform(-pi, pi)
         dx = cos(phi)
         dy = sin(phi)
-        xdata1[particle] += dx
-        ydata1[particle] += dy
-    #plt.Circle((circle_point[0], circle_point[1]), circle_radius)
+        xdata1[p] += dx
+        ydata1[p] += dy
+    # plt.Circle((circle_point[0], circle_point[1]), circle_radius)
     ln1.set_data(xdata1, ydata1)
     return ln1,
 
 
 def update_p2(frame):
-    n = 5
+    n = 0
 
-   # for particle in range(num_particles):
-       # xdata1[particle] += dx
-       # ydata1[particle] += dy
+    for p in range(num_particles):
+        d = sqrt((xdata1[p] - point[0]) ** 2 + (ydata1[p] - point[1]) ** 2)
+        if d <= radius:
+            n += 1
 
+    xdata2.append(frame)
     ydata2.append(n)
     ln2.set_data(xdata2, ydata2)
     return ln2,
 
 
-ani1 = FuncAnimation(figure, update_p1, frames=np.linspace(0, num_steps, num_steps+1),
-                    init_func=init_p1, interval=2000, blit=True, repeat=False)
+ani1 = FuncAnimation(figure, update_p1, frames=np.linspace(0, num_steps, num_steps + 1),
+                     init_func=init_p1, interval=interval, blit=True, repeat=False)
 
-ani2 = FuncAnimation(figure, update_p2, frames=np.linspace(0, num_steps, num_steps+1),
-                    init_func=init_p2, interval=2000, blit=True, repeat=False)
+ani2 = FuncAnimation(figure, update_p2, frames=np.linspace(0, num_steps, num_steps + 1),
+                     init_func=init_p2, interval=interval, blit=True, repeat=False)
 
+p1.grid()
+p2.grid()
 plt.show()
